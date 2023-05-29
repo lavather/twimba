2b267e306cc935bb4bbd1e8b99c31aa0b3b6e3e7
lavather <114093231+lavather@users.noreply.github.com>
2023-05-29 23:02:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { tweetsData } from "./data.js";
 import {v4 as uuidv4} from "https://jspm.dev/uuid";
-
+console.log("these changes come quick")
 document.addEventListener("click", function(e){
 
     if(e.target.dataset.like){
